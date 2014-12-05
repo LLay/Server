a2d4b58c45af770423cfafbfe0c9f4da7d400ef3
Lay Lyon <g3laylyo@cdf.toronto.edu>
2014-12-05 15:16:11 -0500

    deleting unecessary files

--- a4/calendar.h
+++
@@ -1,5 +0,0 @@
-// #ifndef CALENDAR_H
-// #define CALENDAR_H
-// int process_arg(char command, Calendar **calendar_list_ptr, User **user_list_ptr);
-// void error(char *msg);
-// #endif
--- a4/test.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <ctype.h>
-#include <string.h>
-#include <stdlib.h>
-
-int main(int argc, char **argv) {
-	printf("%s\n", sizeof);
-	return 0;
-}
